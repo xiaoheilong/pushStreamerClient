b4a5418af29f9d0d83f06cc149ff551c23ae5cf2
Zhen Yunlong <ylong@ninedvr.com>
2021-01-26 15:34:49 +0800

    optimaze

---
+++ dumpfileprograme.h
@@ -0,0 +1,35 @@
+﻿#ifndef DUMPFILEPROGRAME_H
+#define DUMPFILEPROGRAME_H
+
+
+class DumpFilePrograme
+{
+public:
+    DumpFilePrograme();
+};
+
+#endif // DUMPFILEPROGRAME_H#
+#ifndef CCRASHSTACK_H
+#define CCRASHSTACK_H
+
+#include <windows.h>
+#include <QString>
+
+class CCrashStack
+{
+private:
+    PEXCEPTION_POINTERS m_pException;
+
+private:
+    QString GetModuleByRetAddr(PBYTE Ret_Addr, PBYTE & Module_Addr);
+    QString GetCallStack(PEXCEPTION_POINTERS pException);
+    QString GetVersionStr();
+    bool GetHardwareInaformation(QString &graphics_card, QString &sound_deivce);
+
+public:
+    CCrashStack(PEXCEPTION_POINTERS pException);
+
+    QString GetExceptionInfo();
+};
+
+#endif // CCRASHSTACK_H
